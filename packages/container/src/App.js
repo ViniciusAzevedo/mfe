8ad08e21dba983650ef7a0117d6b73eba69aa895
vinicius.melo <vinicius.melo@MacBook-Pro-de-viniciusmelo.local>
2021-11-18 09:27:56 -0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MarketingApp from "../components/MarketingApp";
+
+// mount is not a React componente because it is a project`s requirement
+// of not making assumptions of a specific framework is used by a remote MF
+// mount should be a pure js function
+// if we ever decide to swap technologies, both sides are free to do it!
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there, this is App talking!</h1>
+      <hr></hr>
+      <MarketingApp />
+    </div>
+  );
+};
