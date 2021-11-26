ffe8288690fb355a8b11d2e070a0675f1dc99c3e
vinicius.melo <vinicius.melo@thoughtworks.com>
2021-11-26 10:15:25 -0300

    added auth subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 
-import MarketingApp from "../components/MarketingApp";
 import Header from "../components/Header";
+import Progress from "../components/Progress";
 
+const MarketingLazy = lazy(() => import("../components/MarketingApp"));
+const AuthLazy = lazy(() => import("../components/AuthApp"));
 // mount is not a React componente because it is a project`s requirement
 // of not making assumptions of a specific framework is used by a remote MF
 // mount should be a pure js function
@@ -18,12 +20,21 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)}></AuthLazy>
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
